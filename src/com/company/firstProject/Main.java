455e93a0dbb195129275a7658614b9857107d27d
Srijeetroy03 <srijeetroy03@gmail.com>
2022-09-05 12:16:39 +0530

    Initial Commit

---
+++ src/com/company/firstProject/Main.java
@@ -0,0 +1,9 @@
+class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hi Srijeet");
+        String name= "Keshav";
+        System.out.println(name.length());
+    }
+}
